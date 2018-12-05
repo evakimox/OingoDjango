2a911c9e10480e9e5672a7d0350f515cdc6b2945
evakimox <zhangjn@bu.edu>
2018-12-05 15:33:58 -0500

    getting back setting

--- oingo/oingo/settings.py
+++ oingo/oingo/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',  # 你的数据123sd24r库名称
+        'NAME': '',  # 你的数据库名称
         'USER': '',  # 你的数据库用户名
         'PASSWORD': '',  # 你的数据库密码
         'HOST': '',  # 你的数据库主机，留空默认为localhost
